ee0ac9db8c77a1f64fb8b655ea9918814b0779bc
Саматов Ансар Нурланоаич <171016665@step.kz>
2024-12-10 21:39:04 +0500

    Profile, login functions are added.

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/profile/', views.profile, name='profile'),
+    path('accounts/login/', views.BBLoginView.as_view(), name='login'),
     path('<str:page>', views.other_page, name='other'),
     path('', views.index, name='index')
 ]
